1223ddf0e16abb01030534fc0edbbeb25310e19c
AntonPanchenko <a.a.panchenkoo@gmail.com>
2020-03-28 19:20:34 +0200

    add sorting on back

--- backend/modules/animals/animal.service.js
+++ backend/modules/animals/animal.service.js
@@ -1,4 +1,4 @@
-const { Op } = require('sequelize');
+const sequelize = require('sequelize');
 const animalModel = require('./animal.model');
 const { NotFound } = require('../../common/exceptions/');
 
@@ -10,6 +10,38 @@ class AnimalService {
 
     async getPage(offset, limit){
         return animalModel.findAll({offset: offset, limit: limit});
+    }
+
+    async getPageSortByPriceDown(offset, limit){
+        return animalModel.findAll({
+            offset: offset,
+            limit: limit,
+            order: [['price', 'DESC']]
+        })
+    }
+
+    async getPageSortByPriceUp(offset, limit){
+        return animalModel.findAll({
+            offset: offset,
+            limit: limit,
+            order: [['price']]
+        })
+    }
+
+    async getPageSortByAgeDown(offset, limit){
+        return animalModel.findAll({
+            offset: offset,
+            limit: limit,
+            order: [['birth_date', 'DESC']]
+        })
+    }
+
+    async getPageSortByAgeUp(offset, limit){
+        return animalModel.findAll({
+            offset: offset,
+            limit: limit,
+            order: [['birth_date']]
+        })
     }
 
     async getOneById(id){
--- backend/modules/pages/pages.controller.js
+++ backend/modules/pages/pages.controller.js
@@ -8,10 +8,43 @@ class PagesController {
     async getPage(req, res, next){
         try{
             const offset = limit * (req.params.id - 1);
+            const sortBy = req.query.sort;
+            if(!sortBy) {
 
-            const pageData = await animalService.getPage(offset, limit);
+                const pageData = await animalService.getPage(offset, limit);
 
-            res.json(pageData);
+                res.json(pageData);
+            }
+            if(sortBy){
+                switch (sortBy) {
+                    case 'priceDown':{
+                        const pageSortData = await animalService.getPageSortByPriceDown(offset, limit);
+                        res.json(pageSortData);
+                        break;
+                    }
+                    case 'priceUp':{
+                        const pageSortData = await animalService.getPageSortByPriceUp(offset, limit);
+                        res.json(pageSortData);
+                        break;
+                    }
+                    case 'ageDown':{
+                        const pageSortData = await animalService.getPageSortByAgeDown(offset, limit);
+                        res.json(pageSortData);
+                        break;
+                    }
+                    case 'ageUp':{
+                        const pageSortData = await animalService.getPageSortByAgeUp(offset, limit);
+                        res.json(pageSortData);
+                        break;
+                    }
+                    default:{
+                        //Error mb?
+                        break;
+                    }
+                }
+
+
+            }
         }catch(e){
             next(e);
         }
